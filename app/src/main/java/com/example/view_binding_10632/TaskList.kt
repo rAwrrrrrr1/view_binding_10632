6a0348ceccefef173826103a3d4ec3841444074a
rAwrrrrrr1 <bet14a@gmail.com>
2022-09-13 08:45:53 +0700

    GD4_D_10632(view binding)

---
+++ app/src/main/java/com/example/view_binding_10632/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10632
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("chapter 1", "Bangun","Aktivitas keseharian"),
+        Task("chapter 2", "Tugas", "Mengerjakantugas-tugas yanga ada"),
+        Task("chapter 3", "Siang", "Mencari makan siang karena lapar"),
+        Task("chapter 4", "Sore", "Mencari hiburan dengan main"),
+        Task("chapter 5", "Malam", "Kembali ke tempat tidur"),
+    )
+}
